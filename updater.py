e96523e5a0633efb58521e9bccc4632d1067125b
Beelzebub2 <ricardorocamartins2004@gmail.com>
2023-08-17 00:51:11 +0100

    v1.2

--- updater.py
+++ updater.py
@@ -5,7 +5,7 @@ import requests
 import os
 import shutil
 from zipfile import ZipFile
-from colorama import Fore
+from colorama import Fore, Style
 import themes as t
 
 
@@ -86,34 +86,39 @@ theme = theme_mapping.get(theme, t.fire)
 
 
 def search_for_updates():
-    clear()
-    set_console_title("Checking For Updates. . .")
+    try:
+        clear()
+        set_console_title("Checking For Updates. . .")
 
-    if online_version != current_version:
-        set_console_title("New Update Found!")
-        print(t.new_update(theme, current_version))
-        choice = input(
-            f"{Fore.GREEN}[{Fore.YELLOW}>>>{Fore.GREEN}] {Fore.RESET}You want to update to the latest version? (Y to update): {Fore.RED}"
-        )
-
-        if choice.lower() == "y" or choice.lower() == "yes":
-            print(f"{Fore.WHITE}\nUpdating. . .")
-            set_console_title("Updating...")
-            new = requests.get(
-                f"https://github.com/Beelzebub2/BeelProxy/archive/refs/heads/main.zip"
+        if online_version != current_version:
+            set_console_title("New Update Found!")
+            print(t.new_update(theme, current_version))
+            choice = input(
+                f"{Fore.GREEN}[{Fore.YELLOW}>>>{Fore.GREEN}] {Fore.RESET}You want to update to the latest version? (Y to update): {Fore.RED}"
             )
-            with open("BeelProxyr.zip", "wb") as zipfile:
-                zipfile.write(new.content)
-            with ZipFile("BeelProxyr.zip", "r") as filezip:
-                filezip.extractall()
-            os.remove("BeelProxyr.zip")
-            cwd = os.getcwd() + "\\BeelProxy-main"
-            shutil.copytree(cwd, os.getcwd(), dirs_exist_ok=True)
-            shutil.rmtree(cwd)
-            config_handler.set("Version", online_version)
-            clear()
-            print(t.updated(theme))
-            set_console_title(f"Update Successfully Finished!")
-            time.sleep(2)
-            os.system("start.bat")
-            os._exit(0)
+
+            if choice.lower() == "y" or choice.lower() == "yes":
+                print(f"{Fore.WHITE}\nUpdating. . .")
+                set_console_title("Updating...")
+                new = requests.get(
+                    f"https://github.com/Beelzebub2/BeelProxy/archive/refs/heads/main.zip"
+                )
+                with open("BeelProxyr.zip", "wb") as zipfile:
+                    zipfile.write(new.content)
+                with ZipFile("BeelProxyr.zip", "r") as filezip:
+                    filezip.extractall()
+                os.remove("BeelProxyr.zip")
+                cwd = os.getcwd() + "\\BeelProxy-main"
+                shutil.copytree(cwd, os.getcwd(), dirs_exist_ok=True)
+                shutil.rmtree(cwd)
+                config_handler.set("Version", online_version)
+                clear()
+                print(t.updated(theme))
+                set_console_title(f"Update Successfully Finished!")
+                time.sleep(2)
+                os.system("start.bat")
+                os._exit(0)
+    except KeyboardInterrupt:
+        print(
+            f"\n{Style.BRIGHT + Fore.LIGHTBLUE_EX}KeyboardInterrupt detected. Exiting gracefully.{Fore.RESET}"
+        )
